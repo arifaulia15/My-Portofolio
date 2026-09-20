834920554b831e6b981fab9f4bca750c6e237473
arifaulia15 <arifaulia1535@gmail.com>
2026-09-21 00:35:34 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,74 @@
+/* =====================================================
+   MENU MOBILE
+===================================================== */
+
+const menuBtn = document.getElementById("menuBtn");
+const navMenu = document.getElementById("navMenu");
+
+
+// Ketika tombol menu diklik
+menuBtn.addEventListener("click", function () {
+
+    navMenu.classList.toggle("show");
+
+});
+
+
+/* =====================================================
+   TUTUP MENU SETELAH LINK DIKLIK
+===================================================== */
+
+const navLinks = document.querySelectorAll("#navMenu a");
+
+navLinks.forEach(function (link) {
+
+    link.addEventListener("click", function () {
+
+        navMenu.classList.remove("show");
+
+    });
+
+});
+
+
+/* =====================================================
+   MENU AKTIF SESUAI SECTION
+===================================================== */
+
+const sections = document.querySelectorAll("section[id]");
+
+
+window.addEventListener("scroll", function () {
+
+    let currentSection = "";
+
+    sections.forEach(function (section) {
+
+        const sectionTop = section.offsetTop - 150;
+
+        if (window.scrollY >= sectionTop) {
+
+            currentSection = section.getAttribute("id");
+
+        }
+
+    });
+
+
+    navLinks.forEach(function (link) {
+
+        link.classList.remove("active");
+
+
+        if (
+            link.getAttribute("href") ===
+            "#" + currentSection
+        ) {
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
